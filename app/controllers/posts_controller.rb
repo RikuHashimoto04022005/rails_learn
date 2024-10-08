87781b7dcfd103fb7c8940444f1c6185b81256f6
HashimotoRiku <riku-hashimoto@jinjib.co.jp>
2024-10-08 16:56:10 +0900

    投稿サイト

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to @post, notice: "投稿が作成されました。"
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.new(post_params)
+        if @post.update(post_params)
+            redirect_to @post, notice: "投稿が更新されました。"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path, notice: "投稿が削除されました。"
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)  # Strong Parametersで安全な属性のみ許可
+    end
+end
